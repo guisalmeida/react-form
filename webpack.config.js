671288f7f9c44cc42f89214d616cbac52feed3b1
Guilherme Almeida <guisalmeida.dev@gmail.com>
2022-06-30 16:19:31 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|png|jpg|svg|gif|ico)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+          }
+        }],
+      },
+      {
+        test: /.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+}
